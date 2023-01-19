b949550a0624bfac9c819d83761162ca021c0821
Alexander Blunt <55701570+Alexander-Blunt@users.noreply.github.com>
2023-01-19 15:03:53 +0000

    Implemented Service class for requesting trivia for a random number

---
+++ APITestFramework/APIFramework/SingleRandomTriviaService.cs
@@ -0,0 +1,28 @@
+﻿using Newtonsoft.Json;
+
+namespace APIFramework;
+
+public class SingleRandomTriviaService
+{
+    #region Parameters
+    public CallManager CallManager { get; set; }
+    public Model? ResponseContent { get; private set; }
+    public string? ResponseString { get; private set; }
+    #endregion
+
+    #region Constructors
+    public SingleRandomTriviaService(CallManager callManager)
+    {
+        CallManager = callManager;
+    }
+    #endregion
+
+    #region Methods
+    public async Task MakeRequestAsync()
+    {
+        ResponseString = await CallManager.MakeRequestAsync("random/trivia");
+
+        ResponseContent = JsonConvert.DeserializeObject<Model>(ResponseString);
+    }
+    #endregion
+}
