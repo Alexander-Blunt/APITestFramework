5564a041fa26d6564738208d3537dae2428e4550
Scot Morrison <SMorrison@spartaglobal.com>
2023-01-19 14:48:04 +0000

    Added model class properties. Added ValidDate tests.

--- APITestFramework/APIFramework/DataHandling/Model.cs
+++ APITestFramework/APIFramework/DataHandling/Model.cs
@@ -1,6 +1,16 @@
-﻿namespace APIFramework;
+﻿
+using Newtonsoft.Json;
 
 public class Model
 {
-
+    [JsonProperty("text")]
+    public string Text { get; set; }
+    [JsonProperty("year")]
+    public int Year { get; set; }
+    [JsonProperty("number")]
+    public int Number { get; set; }
+    [JsonProperty("found")]
+    public bool Found { get; set; }
+    [JsonProperty("type")]
+    public string Type { get; set; }
 }
